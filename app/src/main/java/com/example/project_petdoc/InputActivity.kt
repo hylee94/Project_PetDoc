65a79822249d2ba43e4f5fb1c563b28c70265d97
pmini15 <0115wjdals@naver.com>
2024-10-30 16:01:53 +0900

    로그인, 회원가입 화면

--- app/src/main/java/com/example/project_petdoc/InputActivity.kt
+++ app/src/main/java/com/example/project_petdoc/InputActivity.kt
@@ -1,29 +1,23 @@
 package com.example.project_petdoc
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.project_petdoc.databinding.ActivityMainBinding
+import com.example.project_petdoc.databinding.ActivityInputBinding
 
-class MainActivity : AppCompatActivity() {
+class InputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //(R.layout.activity_input)
+        val binding = ActivityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.btnJoin.setOnClickListener {
-            val intent = Intent(this, InputActivity::class.java)
-            startActivity(intent)
         }
     }
 }
