dbf4693844f4c23b65068bdb6e248bd598db1fdd
hylee94 <gmlsendol@gmail.com>
2024-11-01 14:49:44 +0900

    1

--- app/src/main/java/com/example/project_petdoc/Member/MainActivity.kt
+++ app/src/main/java/com/example/project_petdoc/Member/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.project_petdoc.Member
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/project_petdoc/ProfileActivity.kt
+++ app/src/main/java/com/example/project_petdoc/ProfileActivity.kt
@@ -134,4 +134,5 @@ class ProfileActivity : AppCompatActivity() {
             .create()
             .show()
     }
+
 }
